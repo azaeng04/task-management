76ff4241c6fcf03a604194e611e70d6f42c87dc7
Azariah <m04sengy@gmail.com>
2022-11-16 17:24:18 +0000

    chore: unit test update

--- src/tasks/tasks.repository.test.ts
+++ src/tasks/tasks.repository.test.ts
@@ -1,27 +1,94 @@
 import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, SelectQueryBuilder } from 'typeorm';
+import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
 import { TasksRepository } from './tasks.repository';
 
 describe(`TasksRepository`, () => {
-  let tasksRepository: TasksRepository;
+  let taskRepository: TasksRepository;
   let taskRepo: Repository<Task>;
+  let selectQb;
 
   beforeEach(async () => {
+    selectQb = {
+      where: jest.fn(),
+      getMany: jest.fn(),
+      andWhere: jest.fn(),
+    };
+
     const module = await Test.createTestingModule({
       providers: [
         TasksRepository,
         {
           provide: getRepositoryToken(Task),
-          useValue: {},
+          useValue: {
+            createQueryBuilder: jest.fn().mockReturnValue(selectQb),
+          },
         },
       ],
     }).compile();
 
-    tasksRepository = module.get(TasksRepository);
+    taskRepository = module.get(TasksRepository);
     taskRepo = module.get(getRepositoryToken(Task));
   });
+  test('should have createQueryBuilder called once with "task"', async () => {
+    const createQueryBuilderSpy = jest.spyOn(taskRepo, 'createQueryBuilder');
 
-  test('placeholder', async () => {});
+    await taskRepository.getTasks({}, null);
+
+    expect(createQueryBuilderSpy).toHaveBeenCalledTimes(1);
+    expect(createQueryBuilderSpy).toHaveBeenCalledWith('task');
+  });
+
+  test('should have createQueryBuilder called once with "task"', async () => {
+    const createQueryBuilderSpy = jest.spyOn(taskRepo, 'createQueryBuilder');
+
+    await taskRepository.getTasks({}, null);
+
+    expect(createQueryBuilderSpy).toHaveBeenCalledTimes(1);
+    expect(createQueryBuilderSpy).toHaveBeenCalledWith('task');
+  });
+  describe(`status and search undefined and null user`, () => {
+    test('should have query.where called once', async () => {
+      const selectQbWhereSpy = jest.spyOn(selectQb, 'where');
+
+      await taskRepository.getTasks({}, null);
+
+      expect(selectQbWhereSpy).toHaveBeenCalledTimes(1);
+    });
+
+    test('should have query.where called with null user', async () => {
+      const selectQbWhereSpy = jest.spyOn(selectQb, 'where');
+
+      await taskRepository.getTasks({}, null);
+
+      expect(selectQbWhereSpy).toHaveBeenCalledWith({ user: null });
+    });
+  });
+
+  describe(`status defined and search undefined and null user`, () => {
+    test('should call query.andWhere once', async () => {
+      const tasksFilterDto = { status: TaskStatus.OPEN };
+      const selectQbAndWhereSpy = jest.spyOn(selectQb, 'andWhere');
+
+      await taskRepository.getTasks(tasksFilterDto, null);
+
+      expect(selectQbAndWhereSpy).toHaveBeenCalledTimes(1);
+    });
+
+    test('should call query.andWhere to have been called with status set to open and "task.status = :status"', async () => {
+      const tasksFilterDto = { status: TaskStatus.OPEN };
+      const selectQbAndWhereSpy = jest.spyOn(selectQb, 'andWhere');
+
+      await taskRepository.getTasks(tasksFilterDto, null);
+
+      expect(selectQbAndWhereSpy).toHaveBeenCalledWith(
+        'task.status = :status',
+        { status: TaskStatus.OPEN },
+      );
+    });
+  });
+
+  describe(`status and search defined and null user`, () => {});
 });
